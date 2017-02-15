df63676aedbaf5b731f9edab2ec9d1678ee6274d
jakubfiala <mu201jf@gold.ac.uk>
2017-02-15 16:41:10 +0000

    some functional goodness on rms

--- extractors/rms.rs
+++ extractors/rms.rs
@@ -1,11 +1,6 @@
 pub fn compute(signal : &Vec<f64>) -> f64 {
-  let mut sum = 0_f64;
+  let sum = signal.iter().fold(0_f64, |acc, &sample| acc + sample.powf(2_f64));
+  let mean = sum / signal.len() as f64;
 
-  for sample in signal {
-    sum += sample.powf(2_f64);
-  }
-
-  sum /= signal.len() as f64;
-
-  sum.sqrt()
+  mean.sqrt()
 }
