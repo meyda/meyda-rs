040ec64ec0e812844f42b9076158623180f50aab
jakubfiala <mu201jf@gold.ac.uk>
2017-02-16 21:36:44 +0000

    added amp_spectrum to public API

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,7 @@ pub fn get_rms(signal : &Vec<f64>) -> f64 {
 pub fn get_power_spectrum(signal : &Vec<f64>) -> Vec<f64> {
   extractors::power_spectrum::compute(signal)
 }
+
+pub fn get_amp_spectrum(signal : &Vec<f64>) -> Vec<f64> {
+  extractors::amp_spectrum::compute(signal)
+}
