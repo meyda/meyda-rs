7b02acca7bcef8cf8761de80ca1070789b62f604
jakubfiala <mu201jf@gold.ac.uk>
2017-02-18 19:56:33 +0000

    using powi instead of powf

--- src/extractors/power_spectrum.rs
+++ src/extractors/power_spectrum.rs
@@ -10,7 +10,7 @@ pub fn compute(signal : &Vec<f64>) -> Vec<f64> {
   let amp_spec: Vec<f64> = amp_spectrum::compute(signal);
 
   let pow_spec: Vec<f64> = amp_spec.iter()
-    .map(|bin| bin * bin)
+    .map(|bin| bin.powi(2))
     .into_iter().collect();
 
   return pow_spec;
--- src/extractors/rms.rs
+++ src/extractors/rms.rs
@@ -6,7 +6,7 @@
  * @return     the RMS value (f64)
  */
 pub fn compute(signal : &Vec<f64>) -> f64 {
-  let sum = signal.iter().fold(0_f64, |acc, &sample| acc + sample.powf(2_f64));
+  let sum = signal.iter().fold(0_f64, |acc, &sample| acc + sample.powi(2));
   let mean = sum / signal.len() as f64;
 
   mean.sqrt()
