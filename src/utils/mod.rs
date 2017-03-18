bfb63ce3fc5f7d0a73a506cbb5cfb3b69b49fad0
jakubfiala <mu201jf@gold.ac.uk>
2017-03-18 13:52:55 +0000

    added bark scale generator

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,16 @@ pub fn mu(exp: i32, vec: &Vec<f64>) -> f64 {
 
   fraction.0 / fraction.1
 }
+
+fn bark_map(bin: f64) -> f64 {
+  13.0 * (bin / 1315.8).atan() + 3.5 * (bin / 7518.0).powi(2).atan()
+}
+
+pub fn bark_scale(length: usize, sample_rate: f64, buffer_size: f64) -> Vec<f64> {
+  let scale = vec![0; length];
+
+  scale.iter()
+    .enumerate()
+    .map(|s| bark_map(s.0 as f64 * sample_rate / buffer_size))
+    .collect()
+}
